1e6288509c54265ec123c6b0f1d17a0838657ecc
Bhupender Singh <bbhupender100@gmail.com>
2025-08-13 12:51:29 +0530

    change mongouri

--- server/src/routes/shortUrlRoute.js
+++ server/src/routes/shortUrlRoute.js
@@ -5,7 +5,7 @@ import getAllUrls from '../controllers/getAllUrls.js';
 import deleteUrl from "../controllers/deleteUrl.js";
 // import getUserurls from "../controllers/getUserUrls.js";
 import authMiddleware from "../middlewares/authMiddleware.js";
-// import getUserUrls from "../controllers/getUserUrls.js";
+import getUserUrls from "../controllers/getUserUrls.js";
 const router = express.Router();
 // router.get('/',getAllUrls);
 router.get("/", authMiddleware, getUserUrls);

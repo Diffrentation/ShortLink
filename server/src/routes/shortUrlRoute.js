c57a420b61a0a5321010d6cc66e8d96b293d063b
Bhupender Singh <bbhupender100@gmail.com>
2025-08-13 15:31:52 +0530

    update

--- server/src/routes/shortUrlRoute.js
+++ server/src/routes/shortUrlRoute.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { shortUrl } from "../controllers/shortUrl.js";
 import { redirectUrl } from "../controllers/redirectUrl.js";
-import getAllUrls from '../controllers/getAllUrls.js';
 import deleteUrl from "../controllers/deleteUrl.js";
 import authMiddleware from "../middlewares/authMiddleware.js";
 import getUserUrls from "../controllers/getUserUrls.js";
